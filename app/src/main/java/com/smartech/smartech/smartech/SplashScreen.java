752ee2170e0a20c35b4dadc83e28a157efa99aaa
Prasanth NV <prasanthnvenugopal@gmail.com>
2018-04-07 22:33:07 +0530

    initial commit

--- app/src/main/java/com/smartech/smartech/smartech/SplashScreen.java
+++ app/src/main/java/com/smartech/smartech/smartech/SplashScreen.java
@@ -1,7 +1,11 @@
 package com.smartech.smartech.smartech;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Button btn = findViewById(R.id.btn_click);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
